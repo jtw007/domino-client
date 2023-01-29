5eedfad681664dba0b76a9a8ec1bd431ed261e46
jtw007 <joshwu095@gmail.com>
2023-01-29 14:30:57 -0800

    re-committing

--- src/components/pages/new-post/NewPost.jsx
+++ src/components/pages/new-post/NewPost.jsx
@@ -4,7 +4,6 @@ import { useNavigate, Navigate } from "react-router-dom"
 import jwt_decode from 'jwt-decode'
 
 export default function NewPost(){
-<<<<<<< HEAD
     //state that holds the value the user has typed
     const [form, setForm] = useState({
         //initialize all values as empty strings because this is a new post
@@ -37,31 +36,6 @@ export default function NewPost(){
     }
 
     
-=======
-        //state that holds the value the user has typed
-        const [form, setForm] = useState({
-            //initialize all values as empty strings because this is a new post
-            title: '',
-            content: '',
-        })
-        // console.log(process.env.REACT_APP_SERVER_URL)
-        
-        //invoke useNavigate hook to get a navigate function to use
-        const navigate = useNavigate()
-
-        //submit handler function that posts the form data from state to the backend
-        const handleSubmit = (e) => {
-            e.preventDefault()
-            //take form data from the state, post it to the backend with axios
-            axios.post(`${process.env.REACT_APP_SERVER_URL}/posts`, form)
-                .then(response => {
-                    console.log(response.data)
-                    //once backend gets back to use, navigate to the '/' (home route) to see all the posts
-                    navigate('/')
-                })
-                .catch(console.warn) //for errors 
-        }
->>>>>>> main
     return(
         <div className="form-container">
 
