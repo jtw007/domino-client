50afb32d0933193e7231acfa836a8e5ee714ff9c
jtw007 <joshwu095@gmail.com>
2023-01-29 21:08:15 -0800

    added comments in lines 18, 19, 24, 30

--- src/components/pages/new-post/NewPost.jsx
+++ src/components/pages/new-post/NewPost.jsx
@@ -15,15 +15,19 @@ export default function NewPost(){
     
     //invoke useNavigate hook to get a navigate function to use
     const navigate = useNavigate()
+    //localStorage = web storage object that allows JS sites and apps to keep key-value pairs in web browser with no expiration date; enables developers to store and retrieve data in the browser - not good practice since data will be lost if the user clears cache
+        //in this case, we are storing the jwt 
     const token = localStorage.getItem('jwt')
     if(!token) {
         return <Navigate to="/login" />
     }
+    // decodes the token
     const decoded = jwt_decode(token)
     // console.log(decoded.id)
     //submit handler function that posts the form data from state to the backend
     const handleSubmit = (e) => {
         e.preventDefault()
+        //adds user ID to form
         setForm({ ...form, user: decoded.id })
         // take form data from the state, post it to the backend with axios
         axios.post(`${process.env.REACT_APP_SERVER_URL}/posts`, form)
@@ -34,7 +38,6 @@ export default function NewPost(){
             })
             .catch(console.warn) //for errors 
     }
-
     
     return(
         <div className="form-container">
@@ -43,18 +46,20 @@ export default function NewPost(){
                 <div className="form-sheet">
 
                     <p className="new-post">Create a Post</p>
-                    <label htmlFor="title">Title:</label>
+                    <label htmlFor="title"></label>
                         <input 
                             type='text'
                             id='title'
+                            placeholder="Title"
                             title={form.title}
                             onChange={e => setForm({ ...form, title: e.target.value})}
                         />
 
-                    <label htmlFor="content">What's on your mind?</label>
+                    <label htmlFor="content"></label>
                         <input 
                             type='text'
                             id='content'
+                            placeholder="What's on your mind?"
                             title={form.content}
                             onChange={e => setForm({ ...form, content: e.target.value})}
                         />
