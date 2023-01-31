7491da0e9554e52b89238ac85a65bd61a8801f32
dudahamilton <mariaehamilton28@gmail.com>
2023-01-30 23:10:48 -0800

    part of the edit button functionality is done now I just need a text area

--- src/components/pages/PostDetails.jsx
+++ src/components/pages/PostDetails.jsx
@@ -42,7 +42,7 @@ export default function PostDetails({currentUser}) {
     }
 
     const handleDeleteClick = async (commentId) => {
-        
+        // grabbing the post by comment id and deleting it through axios
         await axios.delete(`${process.env.REACT_APP_SERVER_URL}/posts/${id}/comment/${commentId}`)
             
         .then(response => {
@@ -51,7 +51,18 @@ export default function PostDetails({currentUser}) {
             
        .catch(console.warn)
     }
+
+    const handleEditClick = async (commentId) => {
+        // grabbing the comment by id and 'editing it' with axios
+        await axios.put(`${process.env.REACT_APP_SERVER_URL}/posts/${id}/comment/${commentId}`)
+        // calling fetchPost to 'refresh' the page (refresh might not be the right word but it's what happens in this case)
+        .then(response => {
+            fetchPost()
+        })
+        .catch(console.warn)
+    }
     const commentForm = (
+        // calls handleSubmit function every time the user clicks on the submit button
         <form onSubmit={handleSubmit} htmlFor='comment'>
         <div> 
             <label htmlFor="comment"></label>
@@ -64,6 +75,7 @@ export default function PostDetails({currentUser}) {
         </form>
     )
     const commentComponents = post.comments?.map((comment, idx) => {
+        // ? -> basically some conditional logic like an if else, but here we’re just checking if the post has a property comments
         let ownComment = false
         let buttons = ''
         console.log(currentUser.id, comment)
@@ -71,30 +83,30 @@ export default function PostDetails({currentUser}) {
             buttons =  <button onClick={() => handleDeleteClick(comment._id)}>Delete</button>
             buttons+= <button>Edit</button>
         }
-
-        //console.log(comment)
+        
+        
+        
         return (
           <div key={`comment-${idx}`}>
               <div>
                   {comment.content}
                   {buttons}
-                  
               </div>
           </div>
         )
       })
     return (
         <>
-            <h2>{post.title}</h2>
-            <div>{post.content}
-            {/* <p>{post.user.name}</p> */} </div>
-             <div>
-                 <h3>Comments:</h3>
-                {commentComponents}   
-            </div>  
-           {currentUser && commentForm}
-            
-        </>
+        <h2>{post.title}</h2>
+        <div>{post.content}
+        {/* <p>{post.user.name}</p> */} </div>
+         <div>
+             <h3>Comments:</h3>
+            {commentComponents}   
+        </div>  
+       {currentUser && commentForm}
+        
+    </>
     )
     
     
@@ -103,8 +115,8 @@ export default function PostDetails({currentUser}) {
 // show content ✅
 //show name of user ✅
 // show comments (if no comment: display no comments yet) ✅
-// delete button for each comment (only shows for comments of that specific user)
-// edit comment for each comment (only shows for comments of that specific user)
+// delete button for each comment (only shows for comments of that specific user) ✅
+// edit comment for each comment (only shows for comments of that specific user) ✅
 // area text that displays only when edit button is clicked
 // area text to make a new comment (if user is not logged in: take user to login page) ✅
 
