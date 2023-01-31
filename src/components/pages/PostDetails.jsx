7cf91614369c179c75836cf7ef26711433be0e43
PvrkFvmily <wlsgnl9973@gmail.com>
2023-01-30 16:05:05 -0800

    able to render from needs conditional rendering

--- src/components/pages/PostDetails.jsx
+++ src/components/pages/PostDetails.jsx
@@ -7,7 +7,17 @@ import jwt_decode from 'jwt-decode'
 
 export default function PostDetails() { 
     const [post, setPost] = useState([])
-    // const [comments, setComments] = useState([])
+    
+    // Daniel ________________________________________
+    const [show, setShow] = useState(false)
+    const [edit, setEdit] = useState({
+        title: '',
+        content: ''
+    })
+
+    // _____________________________________________DP
+
+// const [comments, setComments] = useState([])
     const {id} = useParams()
     useEffect(() => {
         const fetchPost = async () =>{
@@ -42,6 +52,20 @@ export default function PostDetails() {
             })
             .catch(console.warn) 
     }
+
+    // Daniel ________________________________________
+    const handleEdit = async e => {
+        e.preventDefault()
+        try {
+            await axios.put(`${process.env.REACT_APP_SERVER_URL}/posts/${id}`, edit)
+        } catch(err) {
+            console.warn(err)
+        }
+    }
+
+    // _____________________________________________DP
+
+
     //   const commentComponents = comments.map((comment, idx) => {
 	// 	return (
 	// 		<div key={`comment-${idx}`}>
@@ -66,7 +90,40 @@ export default function PostDetails() {
             <h3>{post.title}</h3>
             <div>{post.content}
             {/* <p>{post.user.name}</p> */} </div>
-             <div>
+            
+            {/* // Daniel ________________________________________ */}
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <lable htmlFor='title'>Title:</lable>
+                    <input 
+                        type='text'
+                        id='title'
+                        placeholer='title...'
+                        value={form.title}
+                        onChange={e => setEdit({ ...form, title: e.target.value })}
+                    />
+                    <lable htmlFor='content'>Content:</lable>
+                    <input 
+                        type='text'
+                        id='content'
+                        placeholer='content...'
+                        value={form.content}
+                        onChange={e => setEdit({ ...form, content: e.target.value })}
+                    />
+                </form>
+            </div>
+
+
+            <button
+                onClick={() => setShow()}
+                >
+            Edit
+            </button>
+            
+
+            {/* // _____________________________________________DP */}
+
+            <div>
                 {commentComponents}  
                
             </div>  
@@ -95,6 +152,3 @@ export default function PostDetails() {
 // edit comment for each comment (only shows for comments of that specific user)
 // area text that displays only when edit button is clicked
 // area text to make a new comment (if user is not logged in: take user to login page) ✅
-
-
-// name change PostDetail
